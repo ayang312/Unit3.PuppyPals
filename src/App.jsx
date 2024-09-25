e5d4c66fc83c899f17cf806fd7387b65f4c21cb3
Andrew Yang <ayang312@gmail.com>
2024-09-24 23:28:57 -0400

    completed app and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,35 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
   return (
-    <>
-      <div className="App">{
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
+    <div className="App">
+      <h1>Puppy Pals</h1>
+      <div className="puppy-list">
+        {puppies.map((puppy) => (
+          <p
+            onClick={() => setFeatPupId(puppy.id)}
+            key={puppy.id}
+            className={featPupId === puppy.id ? 'active' : ''}
+          >
+            {puppy.name}
+          </p>
+        ))}
       </div>
-    </>
-  )
+
+      {featuredPup && (
+        <div className="featured-puppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
